b1c1acf9e6e184cc70a140d775626b235bef6b17
Tyler Cook <Tyler Cook>
2018-02-02 00:28:51 -0500

    Initial Commit

---
+++ Assets/Scripts/Editor/MyAllPostProcessor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+class MyAllPostprocessor : AssetPostprocessor
+{
+    //static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    //{
+    //    foreach (string str in importedAssets)
+    //    {
+    //        Debug.Log("Reimported Asset: " + str);
+    //    }
+    //    foreach (string str in deletedAssets)
+    //    {
+    //        Debug.Log("Deleted Asset: " + str);
+    //    }
+
+    //    for (int i = 0; i < movedAssets.Length; i++)
+    //    {
+    //        Debug.Log("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
+    //    }
+    //}
+
+    void OnPostprocessModel(GameObject g)
+    {
+        //Debug.Log(g.GetInstanceID());
+        //GameObject go = GameObject.Instantiate(g);
+
+        //Object parentObject = PrefabUtility.FindPrefabRoot(go);
+
+        //string assetPath = AssetDatabase.GetAssetPath(parentObject);
+        Debug.Log("path: " + assetPath);
+        AssetImporter.GetAtPath(assetPath).SetAssetBundleNameAndVariant("testBundle", "");
+    }
+}
---
+++ Assets/Scripts/Editor/PreBuild.cs
@@ -0,0 +1,63 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+using System.Linq;
+
+class PreBuildProcessor
+{
+    [MenuItem("Resources/AssetsBundles/Build Asset Bundles/Windows")]
+    public static void BuildAssetBundles()
+    {
+        Build(BuildTarget.StandaloneWindows64, Application.dataPath + "/StreamingAssets/");
+    }
+
+	public static void Build(BuildTarget target, string path) {
+        //TestVersion();
+
+        //EditorCoroutines.EditorCoroutines.StartCoroutine(DownloadFile(@"teapot.fbx"), this);
+        DownloadFile(@"teapot.fbx");
+
+        AssetDatabase.Refresh(ImportAssetOptions.ForceSynchronousImport);
+
+        Debug.Log("Build Asset Bundles to " + path);
+
+        BuildPipeline.BuildAssetBundles(path,
+                                        BuildAssetBundleOptions.None,
+                                        target);
+
+        //string[] levels = new string[] { "Assets/main.unity" };
+
+        //string buildPath = Application.dataPath.Substring(0, Application.dataPath.Length - 6) + "Builds/test.exe";
+
+        //Debug.Log("Building player to... " + buildPath);
+
+        //BuildPipeline.BuildPlayer(levels, buildPath, BuildTarget.StandaloneWindows64, BuildOptions.ShowBuiltPlayer);
+    }
+
+    static void DownloadFile(string file)
+    {
+
+        Debug.Log("Downloading..." + file);
+
+        string saveLoc = Application.dataPath;
+        string url = @"https://github.com/McNopper/GraphicsEngine/raw/master/GE_Binaries/teapot.fbx";
+
+        WWW www = new WWW(url);
+        
+        while (!www.isDone) {  }
+
+        string newPath = saveLoc + "\\" + file;
+
+        Debug.Log("Saving file..." + newPath);
+
+        File.WriteAllBytes(newPath, www.bytes);
+
+        Debug.Log("Download finished.");
+    }
+}
+#endif
